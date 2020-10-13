ca3f91bc365c8b594157408242553221571330ea
Emily Goeres <goeres123@hotmail.com>
2020-10-13 16:40:36 -0600

    Drops employeeTracker_db if already exists

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # employeetracker
+Title of Project: Employee Tracker
+
+Project Description: For a business owner to be able to pull a list of mployees with their current role, department and salary. The owner can also add, delete an employee, update role/salary to ensure a more planned out outline to start a business. 
+
+Installations: npm init -y, npm install inquirer mysql
+
+
+Steps to complete Employee Tracker:
+1. 
+
+
+
+![Employee Tracker](https://github.com/emilygoeres/ceating-a-quiz-homework/blob/master/codequiz1.png)
+![Employee Tracker](https://github.com/emilygoeres/ceating-a-quiz-homework/blob/master/codequiz1.png)
+![Employee Tracker](https://github.com/emilygoeres/ceating-a-quiz-homework/blob/master/codequiz1.png)
+
+
+Link to Deployed App:
+
+##### Emily Goeres, 
+### Univeristy of Washington
---
+++ employeeTracker.sql
@@ -0,0 +1,47 @@
+-- Drops the employeeTracker_db if it already exists --
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+-- Created the DB "emloyeeTracker_db" (only works on local connections)
+CREATE DATABASE employeeTracker_db;
+
+-- Use the DB employeeTracker_db for all the rest of the script
+USE employeeTracker_db;
+
+-- Create deapartment Table
+create TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create role table
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id) 
+);
+
+-- Create employee table
+create TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGEER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE SET NULL;
+    FOREIGN KEY (manager_id) REFERENCES employee (id) 
+);
+
+
+SELECT first_name, last_name FROM employee INTEGER JOIN role on role_id = role.id;
+SELECT name, id FROM department INNER JOIN role on department.id = department_id;
+SELECT first_name, last_name, title, salary, department_id FROM role INNER JOIN employee on role_id;
+SELECT name, title, salary, fist_name, last_name from department
+INNER JOIN role on department.id = department_id
+INNER JOIN employee on role.id = role_id WHERE WHERE (name=?), [viewbydepartment];
+
+
